270755de1cba232604bd38f4f8ddc48395ee9d8f
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-09-19 19:56:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -49,6 +49,17 @@ class Dog
     dog
   end
 
+  def self.new_from_db(row)
+    id = row[0]
+    name =  row[1]
+    album = row[2]
+    self.new(id, name, album)
+  end
+
+  # def self.new_from_db(row)
+  #   self.new(name: row[1], breed: row[2], id: row[0])
+  # end
+
 
 
 end
