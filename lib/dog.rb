9b1d51e12aaf76a2ae07eb795b8652a4a422bf97
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-09-19 20:15:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -90,13 +90,13 @@ class Dog
         self.new(name: row[1], breed: row[2], id: row[0])
       end
 
-  def self.find_by_name(name)
-    sql = "SELECT * FROM dogs WHERE name = ?"
-
-    DB[:conn].execute(sql,name).map do |row|
-      self.new_from_db(row)
-    end.first
-  end
+  # def self.find_by_name(name)
+  #   sql = "SELECT * FROM dogs WHERE name = ?"
+  # 
+  #   DB[:conn].execute(sql,name).map do |row|
+  #     self.new_from_db(row)
+  #   end.first
+  # end
 
 
 
