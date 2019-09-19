f80420a79da80ee8bcf2944c70a6ed3999baa071
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-09-19 19:49:27 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -37,6 +37,11 @@ class Dog
     end
   end
 
+  def update
+    sql = "UPDATE dogs SET name = ?, breed = ? WHERE id = ?"
+    DB[:conn].execute(sql, self.name, self.breed, self.id)
+  end
+
 
 
 end
